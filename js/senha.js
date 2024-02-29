052962db1c4b0892ab644490fc7a64bc90bff0d5
unknown <davi.cambe@gmail.com>
2024-02-29 17:26:54 -0300

    tela de login

---
+++ js/senha.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const userInput = document.getElementById('userInput');
+    const senhaInput = document.getElementById('senhaInput');
+    const confirmBtn = document.querySelector('.confirmBtn a');
+    const erroMessage = document.getElementById('Erro');
+
+    confirmBtn.addEventListener('click', function (event) {
+        event.preventDefault();
+        
+        const user = userInput.value;
+        const senha = senhaInput.value;
+
+        if (user === 'admin' && senha === 'admin') {
+            
+            window.location.href = './pages/pagBranca.html';
+        } else {
+            
+            erroMessage.style.display = 'Erro';
+        }
+    });
+});
